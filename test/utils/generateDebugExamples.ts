473095d3f55fcd146681d21819b590c71ef76dbe
Daniel Ström <D@nielstrom.se>
2022-02-13 15:48:05 +0100

    Improve pino-debug types

--- test/utils/generateDebugExamples.ts
+++ test/utils/generateDebugExamples.ts
@@ -1,20 +1,11 @@
 #!/usr/bin/env ts-node
 
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="./pino-debug.d.ts" />
-
 import pinoDebug from "pino-debug";
 import debugFactory from "debug";
 import pino from "pino";
 
 const logger = pino({ level: "trace" });
-
-pinoDebug(logger, {
-  auto: true,
-  map: {
-    "*": "trace",
-  },
-});
+pinoDebug(logger, { map: { "*": "trace" } });
 
 const debug = debugFactory("my-test:namespace");
 
