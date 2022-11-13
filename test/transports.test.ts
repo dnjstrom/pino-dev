37ac9c219cc45f93b877453e17aa07067f5fbbf3
Daniel Ström <D@nielstrom.se>
2022-11-13 13:40:57 +0100

    Upgrade pino; remove programmatic support

--- test/transports.test.ts
+++ test/transports.test.ts
@@ -1,9 +1,9 @@
 import { execSync } from "child_process";
 import path from "path";
 
-describe("v7+ Transforms", () => {
+describe("Transports", () => {
   it("formats correctly", () => {
-    const PATH = path.resolve(__dirname, "./v7+-transports.ts");
+    const PATH = path.resolve(__dirname, "./transports.ts");
     const output = execSync(`ts-node ${PATH}`);
     expect(output.toString()).toMatchInlineSnapshot(`
       "00:00:00 Info: Testing 123...
